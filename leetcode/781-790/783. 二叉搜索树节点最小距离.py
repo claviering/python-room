b6caa414dab745d8ca188c93ad7164e411e793da
claviering <claviering@gmail.com>
2020-10-13 10:16:10 +0800

    二叉搜索树节点最小距离

---
+++ leetcode/781-790/783. 二叉搜索树节点最小距离.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+  def minDiffInBST(self, root):
+    nodeList = []
+    self.dfs(root, nodeList)
+    absList = [abs(nodeList[index] - nodeList[index + 1]) for index in range(len(nodeList) - 1)]
+    return min(absList)
+  # 中序遍历二叉树，返回数组
+  def dfs(self, root, nodeList):
+    if not root:
+      return
+    self.dfs(root.left, nodeList)
+    nodeList.append(root.val)
+    self.dfs(root.right, nodeList)
